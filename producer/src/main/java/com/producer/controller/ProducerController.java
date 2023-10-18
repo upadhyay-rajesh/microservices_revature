a508313bf46bb7d0c84c7617171e42dde6070e36
RajeshUpadhyay <RajeshUpadhyay@Rev-PG02REP7>
2023-10-18 16:17:10 -0400

    gfxgf

--- producer/src/main/java/com/producer/controller/ProducerController.java
+++ producer/src/main/java/com/producer/controller/ProducerController.java
@@ -6,11 +6,13 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.producer.entity.Employee;
 
 @RestController
 public class ProducerController {
 	@GetMapping("displayAll")
+	@HystrixCommand(fallbackMethod = "getcircuitfail")
 	public List<Employee> getList(){
 		Employee e1=new Employee();
 		e1.setName("Patric");
@@ -27,6 +29,26 @@ public class ProducerController {
 		List<Employee> ll=new ArrayList<Employee>();
 		ll.add(e1);
 		ll.add(e2);
+		
+		if(ll.size()==2) {
+			throw new RuntimeException();
+		}
+		return ll;
+	}
+	
+	public List<Employee> getcircuitfail() {
+		Employee e2=new Employee();
+		e2.setName("fail");
+		e2.setPassword("fail");
+		e2.setEmail("fail");
+		e2.setAddress("fail");
+		List<Employee> ll=new ArrayList<Employee>();
+		ll.add(e2);
 		return ll;
 	}
 }
+
+
+
+
+
